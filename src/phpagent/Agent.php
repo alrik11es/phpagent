0b7308eabf975230ab0934001f70f4a44990662c
Marcos Sigueros <alrik11es@gmail.com>
2015-02-13 13:21:18 +0100

    Changes that allow to load the config files into the phpagent

--- src/phpagent/Agent.php
+++ src/phpagent/Agent.php
@@ -9,19 +9,57 @@ namespace phpagent;
  */
 class Agent {
 
+    public $extract_methods = array('plugins', 'actions', 'hooks');
+
     public function run()
     {
-        $this->loadConfigIfExists();
+        $config_files = $this->loadJsonConfigFiles();
+        $config = $this->getAgentConfig($config_files);
+        print_r($config);
     }
 
-    public function loadConfigIfExists()
+    /**
+     * Obtain specified config and merge all files config with each other's
+     * @param array $config_files
+     * @return \stdClass
+     */
+    public function getAgentConfig(array $config_files)
     {
-        $dir = scandir(EXEC_PATH);
-        array_shift($dir);
-        array_shift($dir);
-        foreach($dir as $file){
-            echo $file."\n";
+        $config = new \stdClass();
+        foreach($this->extract_methods as $method){
+            $config->$method = array();
         }
+        foreach($config_files as $file){
+            $json = json_decode(file_get_contents($file));
+            if($json !== false){
+                foreach($this->extract_methods as $method){
+                    if(property_exists($json, $method) && is_array($json->$method)){
+                        $config->$method = array_merge($config->$method, $json->$method);
+                    }
+                }
+            }
+        }
+        return $config;
+    }
 
+    /**
+     * Obtain the json files from the script working directory
+     * @return array The array of config files with full path each
+     */
+    public function loadJsonConfigFiles()
+    {
+        $config_files = array();
+        $dir = scandir(EXEC_PATH);
+        if(count($dir) > 2) {
+            array_shift($dir);
+            array_shift($dir);
+            foreach ($dir as $file) {
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                if ($ext == 'json') {
+                    $config_files[] = EXEC_PATH . '/' . $file;
+                }
+            }
+        }
+        return $config_files;
     }
 }
